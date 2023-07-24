149669e5fcb6dcfbd06b43dc2d5a9df20e2f3598
sharleenawinja <awinjasharleen@gmail.com>
2023-07-24 15:01:12 +0300

    creating navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
