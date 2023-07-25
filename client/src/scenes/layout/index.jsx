3c952ec8833a08d262ce4c6b3a953694178aa3df
sharleenawinja <awinjasharleen@gmail.com>
2023-07-25 14:03:51 +0300

    creating products page

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -10,10 +10,7 @@ const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const userId = useSelector((state) => state.global.userId);
-  console.log({ userId });
   const { data } = useGetUserQuery(userId);
-  console.log(data?.name);
-  console.log({ data });
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
